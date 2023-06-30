4b610b5691c62da3b66a7ccba05c3e2b2d73ebc4
PIW <111216062+eatdrink0507@users.noreply.github.com>
2023-06-30 13:46:06 +0900

    Fix 주석 수정
    사용법에 관련된 주석 에서 toclose에 넣어줘야할 값 수정

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,7 +9,7 @@ type Props = {
 // 모달 사용방법
 // 모달을 사용하고 싶은 페이지에서
 //1. import Modal
-//2. return 문 안에 <Modal open={state(boolean 값을 가져야함)} message='메시지' toclose={() => setState(false)} />
+//2. return 문 안에 <Modal open={state(boolean 값을 가져야함)} message='메시지' toclose={setState} />
 
 export const Modal = ({ open, message, toclose }: Props) => {
   return (
