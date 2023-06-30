0593570bb6e22c288c8198f76ceb3e4c11b2a47f
ncb6206 <ncb6206@gmail.com>
2023-06-30 13:11:35 +0900

    Style : prettier 스타일에 맞게 코드 형식 재설정

--- src/hooks/useinput.ts
+++ src/hooks/useinput.ts
@@ -1,8 +1,10 @@
-import { Dispatch, SetStateAction, useCallback, useState } from "react";
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 
 type ReturnType<T> = [T, (e: any) => void, Dispatch<SetStateAction<T>>];
 
-export default function useInput<T extends string>(initialValue: T): ReturnType<T> {
+export default function useInput<T extends string>(
+  initialValue: T
+): ReturnType<T> {
   const [value, setValue] = useState(initialValue);
 
   const handler = useCallback((event: any) => {
