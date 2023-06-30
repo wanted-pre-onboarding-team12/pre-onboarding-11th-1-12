fe21c38907d2d9d951ee9ee084c9f60be613e729
PIW <111216062+eatdrink0507@users.noreply.github.com>
2023-06-30 21:35:16 +0900

    Fix import 수정

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -2,7 +2,7 @@ import styles from './auth.module.scss';
 import { Link, useNavigate } from 'react-router-dom';
 import useInput from '../hooks/useinput';
 import { signInUser } from '../service/auth';
-import { validEmail, validPassword } from '../components/common/utils/vaild';
+import { validEmail, validPassword } from '../components/common/utils/valid';
 
 const SignIn = () => {
   const navigate = useNavigate();
